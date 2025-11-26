b32f5cb4e92ee29846e9c0f334a3b61064651165
David Hunt <dmh89@duke.edu>
2025-11-26 00:04:27 -0500

    refactor: remove AthenaUAVDS and CpslPlotter from package exports.

--- cpsl_datasets/__init__.py
+++ cpsl_datasets/__init__.py
@@ -1,15 +1,11 @@
 """Convenience entry point for the CPSL datasets helpers."""
 
-from .athena_uav_ds import AthenaUAVDS
 from .cpsl_ds import CpslDS
 from .gnn_node_ds import GnnNodeDS
 from .map_handler import MapHandler
-from .plotters.cpsl_plotter import CpslPlotter
 
 __all__ = [
-    "AthenaUAVDS",
     "CpslDS",
     "GnnNodeDS",
     "MapHandler",
-    "CpslPlotter",
 ]
